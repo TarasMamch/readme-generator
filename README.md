af4a6cb358692592a47a3180b4f202bd5951ad25
Taras Mamchits <tmamchitspc@gmail.com>
2022-07-19 23:32:49 -0700

    added all the questions and got the function working

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# readme-generator
+# test1
+
+test2
+# Instalation
+test3
+# Usage
+test4
+# Contributionstest5
+# Questions
+test6<br>test7
--- index.js
+++ index.js
@@ -1,13 +1,104 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs')
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        question: 'What is the name of your project?',
+        answer: ''
+    },
+    {
+        question: 'Breif description of the project',
+        answer: ''
+    },
+    {
+        question: 'How do you install this project?',
+        answer: ''
+    },
+    {
+        question: 'How do you use this project?',
+        answer: ''
+    },
+    {
+        question: 'Who contributed to this project?',
+        answer: ''
+    },
+    {
+        question: 'Enter your Github username',
+        answer: ''
+    },
+    {
+        question: 'Enter your email',
+        answer: ''
+    },
+    {
+        //TODO: add list of options
+        question: 'What licenses do you have?',
+        answer: ''
+    },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: `${questions[0].question}`,
+            name: 'question[0]',
+        },
+        {
+            type: 'input',
+            message: `${questions[1].question}`,
+            name: 'question[1]',
+        },
+        {
+            type: 'input',
+            message: `${questions[2].question}`,
+            name: 'question[2]',
+        },
+        {
+            type: 'input',
+            message: `${questions[3].question}`,
+            name: 'question[3]',
+        },
+        {
+            type: 'input',
+            message: `${questions[4].question}`,
+            name: 'question[4]',
+        },
+        {
+            type: 'input',
+            message: `${questions[5].question}`,
+            name: 'question[5]',
+        },
+        {
+            type: 'input',
+            message: `${questions[6].question}`,
+            name: 'question[6]',
+        },
+        //NOT YET COMPLETE
+        {
+            type: 'list',
+            choices: ['test1', 'test2'],
+            message: `${questions[7].question}`,
+            name: 'question[7]',
+        },
+    ])
+    .then((response) =>
+        writeAnswers(response)
+    )
 
-// TODO: Create a function to initialize app
-function init() {}
+function writeFile() {
+    fs.writeFile('./README.md',
+        `# ${questions[0].answer}\n\n${questions[1].answer}\n# Instalation\n${questions[2].answer}\n# Usage\n${questions[3].answer}\n# Contributions\n${questions[4].answer}\n# Questions\n${questions[5].answer}<br>${questions[6].answer}`,
+        err => {
+            if (err) {
+                console.error(err);
+            }
+        })
+}
 
-// Function call to initialize app
-init();
+function writeAnswers(data) {
+    for (i = 0; i < 7; i++) {
+        questions[i].answer = data.question[i]
+    }
+    writeFile()
+}
